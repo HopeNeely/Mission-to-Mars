2ddd1fe1cace9de4cc224c0bd00d7e54f41f3e39
HopeNeely <hope.neely@gmail.com>
2021-03-21 20:44:56 -0400

    Finished app.py

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -65,8 +65,7 @@ def scrape():
     # ### Mars Hemispheres
     # The USGS Astrogeology site [here](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars) was visited to obtain high resolution images for each of Mars' hemispheres. Python dictionary was created to store the data using the keys `img_url` and `title`. The dictionary was appended with the image url string and the hemisphere title to a list. This list contains one dictionary for each hemisphere.
 
-
-    # Scrape the site using the URL2
+    # Scrape new url
     url4 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url4)
 
@@ -89,14 +88,18 @@ def scrape():
         hemisphere_image_urls.append(hemisphere)
         browser.back()
 
-
-    # Store all scraped data in Python dictionary. 
-    
-
-
-
+ 
+    mars_data = {
+        "latest_news_title": latest_news_title,
+        "latest_news_paragraph": latest_news_paragraph,
+        "featured_image": featured_image,
+        "html_table": html_table,
+        "hemisphere_image_urls": hemisphere_image_urls
+    }
 
     browser.quit()
+
+    return scraped_data
 
 
 
